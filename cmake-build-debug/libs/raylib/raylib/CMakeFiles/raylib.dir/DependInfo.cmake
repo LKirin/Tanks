c2bfe883f4bc2e75fe1bf37466df78cf260f2294
Mak <podlesniy0209@gmail.com>
2023-05-05 19:07:58 +0300

    add Raylib

---
+++ cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/raudio.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/raudio.c.obj"
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/rcore.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/rcore.c.obj"
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/rmodels.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/rmodels.c.obj"
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/rshapes.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/rshapes.c.obj"
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/rtext.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/rtext.c.obj"
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/rtextures.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/rtextures.c.obj"
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/libs/raylib-4.1/src/utils.c" "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/CMakeFiles/raylib.dir/utils.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GRAPHICS_API_OPENGL_33"
+  "PLATFORM_DESKTOP"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../libs/raylib-4.1/src"
+  "../libs/raylib-4.1/src/external/glfw/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/efarinov/Documents/RayLibTemplate/RayLibTemplate/cmake-build-debug/libs/raylib/raylib/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
